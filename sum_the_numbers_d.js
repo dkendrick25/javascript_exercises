226f432a69d41e845eb076263c0fb7bad83e7835
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-05 18:14:21 -0400

    fixed loop to add the numArray[i] to the sum

--- sum_the_numbers_d.js
+++ sum_the_numbers_d.js
@@ -1,8 +1,8 @@
 //sum of numbers of an array
 
 var numArray = [1, 3, 5];
-
-for (var i = 0; i <= numArray.length -  1; i++) {
-  var sum += numArray[i];
-  console.log(sum);
+var sum = 0;
+for (var i = 0; i <= numArray.length - 1; i++) {
+  sum = sum + numArray[i];
 }
+console.log(sum);
