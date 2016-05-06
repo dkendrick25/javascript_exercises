d377d8e4a2fd38c267b870a4a6301588fbd73cc2
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-06 07:52:54 -0400

    Fixed sum loop

--- even_num_II_b.js
+++ even_num_II_b.js
@@ -1,9 +1,10 @@
 // given a num print the sum of the even numbers between 1 and n
 var n = 6;
-var sum;
+var sum = 0;
 for (var i = 1; i <= n; i++) {
   if(i % 2 === 0) {
-    sum += n[i];
+    sum = sum + i;
   }
-  console.log(sum);
 }
+   console.log(sum);
+ 
