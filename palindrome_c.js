c7956f07ec7db070142ab49bb02ccb2d20459490
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-07 19:51:30 -0400

    fixed palindrome exercise

--- palindrome_c.js
+++ palindrome_c.js
@@ -1,9 +1,9 @@
 //given a string print it is a palindrome if it is one
-var str = "amor mora";
-var splitStr = str.split('');
-var reversedStr = splitStr.reverse();
-if (splitStr === reversedStr) {
-  console.log(str + " is a palindrome!")
+var str = "stack cats";
+var str2 = str.replace(/\s/g, '')
+var reversedStr = str2.split('').reverse().join('');
+if (str2 === reversedStr) {
+  console.log(str + ( ' is a palindrome'));
 } else {
-  console.log(str + " is not a palindrome.");
+  console.log(str + ' is not a palindrome');
 }
