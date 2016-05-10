e33e1be3117cadf845a8356c729aa07002c67a57
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-09 20:37:41 -0400

    updated tim's box with functions, still needs work

--- tims_box_function_exercieses.js
+++ tims_box_function_exercieses.js
@@ -1,36 +1,19 @@
-var cols=4;
-var rows=5;
-var rowCounter;
-var i;
-var j;
-var lineOfStars;
-
-// Task 1: print top bun
-lineOfStars = '';
-i = 1;
-
-while (i <= cols) {
-    lineOfStars = lineOfStars + '*';  /* add a star on the end of whatever is in line */
-    i = i + 1;
-}
-console.log(lineOfStars);
-var lineOfStars2 = lineOfStars;
-
-// Task 2: print the meat
-
-lineOfStars = '*';
-i = 1;
-while (i <= cols - 2) {
-  lineOfStars = lineOfStars + ' ';
-  i = i + 1;
-}
-lineOfStars = lineOfStars + '*';
-
-i = 1;
-while (i <= rows -2) {
+function printABox(columns, rows) {
+  var lineOfStars = '';
+  for(var i = 1; i <= columns; i++) {
+    lineOfStars = lineOfStars + "*";
+  }
   console.log(lineOfStars);
-  i = i + 1;
+  var lineOfStars2 = lineOfStars;
+  for (var j = 1; j <= columns; j++) {
+    var starsWithSpaces = "*";
+    starsWithSpaces = starsWithSpaces + ' ';
+  }
+  starsWithSpaces = starsWithSpaces + '*';
+  for (var h = 1; h <= rows - 2; h++) {
+    console.log(starsWithSpaces);
+  }
+  console.log(lineOfStars2)
 }
-
-// Task 3: print bottom bun
-console.log(lineOfStars2);
+printABox(4, 5);
+ //need to print out one more column of spaces on middle row
